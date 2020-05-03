052245e58a0695577296ffee3e357b6312c0cf81
ExciteTaco <ericklima34@gmail.com>
2020-05-03 18:55:04 -0500

    initial commit for MIT intro to python

---
+++ MIT_60001/pset0.py
@@ -0,0 +1,9 @@
+import math
+
+x = int(input('Enter a number for x:\n'))
+y = int(input('Enter a number for y:\n'))
+
+print('X**Y = ' + str(x**y))
+print('log(x) = ' + str(math.log(x, 2)))
+
+# math.log(a,Base)
