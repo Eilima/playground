dcc8b0110edf27c22ea36c87172b62d71f7561d2
ExciteTaco <ericklima34@gmail.com>
2020-05-03 18:58:10 -0500

    Add comments to file and fix numpy import

--- MIT_60001/pset0.py
+++ MIT_60001/pset0.py
@@ -1,9 +1,9 @@
-import math
+import numpy
 
+# Ask for user input and convert string input to int
 x = int(input('Enter a number for x:\n'))
 y = int(input('Enter a number for y:\n'))
-
+# Print the the x to the of y
 print('X**Y = ' + str(x**y))
-print('log(x) = ' + str(math.log(x, 2)))
-
-# math.log(a,Base)
+# Print the log base of 2 of x
+print('log(x) = ' + str(numpy.log2(x)))
