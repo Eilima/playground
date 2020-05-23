e910514a0567b022883cb7f8208c179d14da1c5b
ExciteTaco <ericklima34@gmail.com>
2020-05-22 20:55:49 -0500

    Add constraints that limit both timers inbetween 1 and 60

--- pomodoro-clock/pomodoro-clock/src/Components/Timer.js
+++ pomodoro-clock/pomodoro-clock/src/Components/Timer.js
@@ -13,11 +13,11 @@ export class Timer extends React.Component {
     if (stateName === "focusTime") currentStateVal = this.state.focusTime;
     else currentStateVal = this.state.breakTime;
 
-    if (event.target.value === "+")
+    if (event.target.value === "+" && currentStateVal != 60)
       this.setState({
         [stateName]: currentStateVal + 1,
       });
-    else if (event.target.value === "-")
+    else if (event.target.value === "-" && currentStateVal != 1)
       this.setState({
         [stateName]: currentStateVal - 1,
       });
