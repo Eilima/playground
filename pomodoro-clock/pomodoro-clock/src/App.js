45fa89bd11b99a68693c4723c0b01719b19b3799
ExciteTaco <ericklima34@gmail.com>
2020-05-22 19:45:07 -0500

    Add functional buttons that change the value of state by 1

--- pomodoro-clock/pomodoro-clock/src/App.js
+++ pomodoro-clock/pomodoro-clock/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Clock } from "./components/Clock";
+import { Timer } from "./components/Timer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Timer />
         <Clock />
       </header>
     </div>
