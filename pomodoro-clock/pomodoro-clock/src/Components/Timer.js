9c8edd383285bca71e71fe6f35bf1efbd7cf72fc
ExciteTaco <ericklima34@gmail.com>
2020-05-22 19:46:16 -0500

    Add functional buttons that change the value of state by a value of 1 +/-

--- pomodoro-clock/pomodoro-clock/src/Components/Timer.js
+++ pomodoro-clock/pomodoro-clock/src/Components/Timer.js
@@ -1,7 +1,55 @@
 import React from "react";
 
-export class Clock extends React.Component {
+export class Timer extends React.Component {
+  state = {
+    focusTime: 25,
+    breakTime: 5,
+  };
+
+  timerClick = (event) => {
+    if (event.target.id === "breakTime") {
+      if (event.target.value === "+")
+        this.setState({
+          breakTime: this.state.breakTime + 1,
+        });
+      else
+        this.setState({
+          breakTime: this.state.breakTime - 1,
+        });
+    } else {
+      if (event.target.value === "+")
+        this.setState({
+          focusTime: this.state.focusTime + 1,
+        });
+      else
+        this.setState({
+          focusTime: this.state.focusTime - 1,
+        });
+    }
+  };
+
   render() {
-    return;
+    return (
+      <div>
+        <div>
+          <button onClick={this.timerClick} id="breakTime" value="+">
+            +
+          </button>
+          <h1>{this.state.breakTime}</h1>
+          <button onClick={this.timerClick} id="breakTime" value="-">
+            -
+          </button>
+        </div>
+        <div>
+          <button onClick={this.timerClick} id="focusTime" value="+">
+            +
+          </button>
+          <h1>{this.state.focusTime}</h1>
+          <button onClick={this.timerClick} id="focusTime" value="-">
+            -
+          </button>
+        </div>
+      </div>
+    );
   }
 }
