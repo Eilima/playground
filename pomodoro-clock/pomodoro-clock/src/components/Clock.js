be56d66bafdd0d54586e20c34ecd6ba553d0b265
Eilima <ericklima34@gmail.com>
2020-06-04 21:30:36 -0500

    pomodoro clock changes

---
+++ pomodoro-clock/pomodoro-clock/src/components/Clock.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Timer } from "./Timer";
+
+export class Clock extends React.Component {
+  render() {
+    return (
+      <div>
+        <Timer />
+        <h1>Test</h1>
+      </div>
+    );
+  }
+}
