6dbb25e14ebac2f0b76104dc3725241f475f7361
Eilima <ericklima34@gmail.com>
2020-06-06 21:49:15 -0500

    Edit and improve comments

--- MIT_60001/ps1c.py
+++ MIT_60001/ps1c.py
@@ -1,8 +1,8 @@
 
-#Declaring variables
+# Declaring variables
 semi_annual_raise = .07
 r = .04
 house_cost = 1000000
 down_payment = .25 * house_cost
-#Asking user to insert annual salary and casting it as a float
+# Asking user to insert annual salary and casting it as a float
 annual_salary = float(input("Enter the starting salary:\n"))
