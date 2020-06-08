999ab912cb9cadf1228ab61831624c8bdc1214bd
Eilima <ericklima34@gmail.com>
2020-06-08 17:11:12 -0500

    Add draft of buttons that will control clock component

---
+++ pomodoro-clock/pomodoro-clock/src/components/Controls.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export class Controls extends React.Component {
+
+
+    render() {
+        return (
+            <div className='title-level'>
+                <button>Pause/Play</button>
+                <button>Reset</button>
+            </div>
+        )
+    }
+}
