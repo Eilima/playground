e9e0eedee229761cce61190f0aed1a0ffa8baaf9
Eilima <ericklima34@gmail.com>
2020-06-08 18:38:45 -0500

    Testing onClick command for buttons

--- pomodoro-clock/pomodoro-clock/src/components/Controls.js
+++ pomodoro-clock/pomodoro-clock/src/components/Controls.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 export class Controls extends React.Component {
 
+    onClick = () => {
+        console.log('test')
+    }
+
 
     render() {
         return (
             <div className='title-level'>
-                <button>Pause/Play</button>
-                <button>Reset</button>
+                <button onClick={this.onClick}>Pause/Play</button>
+                <button onClick={this.onClick}>Reset</button>
             </div>
         )
     }
